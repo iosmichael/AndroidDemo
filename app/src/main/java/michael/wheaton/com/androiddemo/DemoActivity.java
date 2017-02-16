df3ab632646bff25ddff6e6b07dccb712a3d3af7
michaelliu642 <michaelliu@iresearch.com.cn>
2017-02-16 15:27:28 -0600

    demo

---
+++ app/src/main/java/michael/wheaton/com/androiddemo/DemoActivity.java
@@ -0,0 +1,17 @@
+package michael.wheaton.com.androiddemo;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by michaelliu on 2/3/17.
+ */
+
+public class DemoActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+    }
+}
---
+++ app/src/main/java/michael/wheaton/com/androiddemo/ProfileActivity.java
@@ -0,0 +1,17 @@
+package michael.wheaton.com.androiddemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by michaelliu on 2/2/17.
+ */
+
+public class ProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
+
+}
