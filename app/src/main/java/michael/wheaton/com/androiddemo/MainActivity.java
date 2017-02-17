b72f358a65666afa9ccd52a68d443fd1f3afc00a
michaelliu642 <michaelliu@iresearch.com.cn>
2017-02-17 00:25:32 -0600

    starter project

--- app/src/main/java/michael/wheaton/com/androiddemo/MainActivity.java
+++ app/src/main/java/michael/wheaton/com/androiddemo/MainActivity.java
@@ -1,10 +1,7 @@
 package michael.wheaton.com.androiddemo;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,20 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        R.id
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showProfileActivity();
-            }
-        });
 
-    }
-
-    void showProfileActivity(){
-        Intent intent = new Intent(this, ProfileActivity.class);
-        startActivity(intent);
     }
 
 }
